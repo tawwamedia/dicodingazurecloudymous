e086819a1171b3666a87ab8b5014ce59c90e31de
cloudymous <sayacloud@gmail.com>
2019-06-26 09:16:20 +0800

    Pusing

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
  <?php
     // PHP Data Objects(PDO) Sample Code:
     try {
-        $conn = new PDO("sqlsrv:server = tcp:cloudymousappserv.database.windows.net,1433; Database = dicodingdb", "cloudymous", "*RmcDwn26#");
+        $conn = new PDO("sqlsrv:server = tcp:cloudymousappserv.database.windows.net,1433; Database = dicodingdb", "cloudymous", "imran*01");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
